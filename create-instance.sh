1c804e6e17521e336999a5cddaad783054e5c56b
vasanthi-dev <vasanthi2dev@gmail.com>
2021-11-03 03:31:21 -0400

    Roboshop project

--- create-instance.sh
+++ create-instance.sh
@@ -12,7 +12,7 @@ CREATE(){
   sleep 5
   IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | xargs )
   #xargs is used to remove double quotes
-  sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+  sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >>/tmp/record.json
 
   aws route53 change-resource-record-sets --hosted-zone-id Z009674929X25UC88DYV9 --change-batch file:///tmp/record.json
 }
