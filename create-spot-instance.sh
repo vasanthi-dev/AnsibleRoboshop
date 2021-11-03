44557585af5a69d74c9ce79fe47bd56133c42455
vasanthi-dev <vasanthi2dev@gmail.com>
2021-11-03 06:49:54 -0400

    Roboshop project

--- create-spot-instance.sh
+++ create-spot-instance.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+UPDATE_DNS_RECORDS(){
+  IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | xargs )
+    #xargs is used to remove double quotes
+    sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
 
+    aws route53 change-resource-record-sets --hosted-zone-id Z009674929X25UC88DYV9 --change-batch file:///tmp/record.json
+}
 CREATE(){
   COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
 
@@ -9,13 +15,10 @@ CREATE(){
     aws ec2 run-instances --launch-template LaunchTemplateId=lt-099eb0b79a90eeba3,Version=3 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>>/dev/null
   else
     echo -e  "\e[31mInstance already exists\e[0m"
+    UPDATE_DNS_RECORDS $1
   fi
   sleep 5
-  IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | xargs )
-  #xargs is used to remove double quotes
-  sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-
-  aws route53 change-resource-record-sets --hosted-zone-id Z009674929X25UC88DYV9 --change-batch file:///tmp/record.json
+  UPDATE_DNS_RECORDS $1
 }
 
 if [ $1 == "all" ]; then
