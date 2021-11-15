337128884742b34aff94f7e7a616ac202433bf78
vasanthi-dev <vasanthi2dev@gmail.com>
2021-11-14 22:07:03 -0500

    Roboshop project

--- create-spot-instance.sh
+++ create-spot-instance.sh
@@ -4,7 +4,7 @@ UPDATE_DNS_RECORDS(){
     #xargs is used to remove double quotes
     sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z009674929X25UC88DYV9 --change-batch file:///tmp/record.json
+    aws route53 change-resource-record-sets --hosted-zone-id Z009674929X25UC88DYV9 --change-batch file:///tmp/record.json | jq &>/dev/null
 }
 CREATE(){
   COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
